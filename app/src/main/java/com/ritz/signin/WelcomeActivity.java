64713f3826edebf5a9dc8445f27794fac4d57a23
Ritz <ritz@Shobhits-MacBook-Air.local>
2020-10-13 01:09:41 +0530

    edit gif in welome activity

--- app/src/main/java/com/ritz/signin/WelcomeActivity.java
+++ app/src/main/java/com/ritz/signin/WelcomeActivity.java
@@ -3,6 +3,9 @@ package com.ritz.signin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        ImageView gifId = findViewById(R.id.gif);
+        Glide.with(this).load(R.drawable.kuma).into(gifId);
     }
 }
